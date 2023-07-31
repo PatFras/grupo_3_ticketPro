0f28cbc7a71f1ef1f532df76e1f03ef57f7a0f0a
brisaguimaraz <brisaguimaraz01@gmail.com>
2023-07-31 02:32:32 -0600

    index done!

---
+++ public/js/swiper-carrousel-desktop.js
@@ -0,0 +1,24 @@
+var swiper = new Swiper(".mySwiper2", {
+    effect: "coverflow",
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows: true,
+    },
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    }, 
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
