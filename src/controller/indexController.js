f2f6328fb122bb5af3c2f7579d51d667ee728be0
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-08-08 17:21:46 -0300

    refactorización a MVC bajo EJS

---
+++ src/controller/indexController.js
@@ -0,0 +1,5 @@
+module.exports = {
+    index : (req, res, next) => {
+        return res.render('index');
+      }
+}
---
+++ src/routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+const usersController = require('../controller/usersController');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/register', usersController.register);
+router.get('/login', usersController.login);
+router.get('/productCart', usersController.productCart);
+router.get('/productDetail', usersController.productDetail);
+
+module.exports = router;
