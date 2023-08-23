9f5e9d004e2d88ed385b3e574b65e7148172a1b7
brisaguimaraz <brisaguimaraz01@gmail.com>
2023-08-22 22:56:00 -0600

    CRUD maquetación

---
+++ src/controller/productsController.js
@@ -0,0 +1,8 @@
+module.exports = {
+    detail : require('./products/detail'),
+    add :require('./products/add'),
+    create : require('./products/create'),
+    edit : require('./products/edit'),
+    update : require('./products/update'),
+    remove : require('./products/remove'),
+}
