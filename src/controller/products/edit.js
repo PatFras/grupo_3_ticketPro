f3c3d37e54b4895db2c77c0e9086c166bbad7b42
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-08-25 17:02:54 -0300

    .

--- src/controller/products/edit.js
+++ src/controller/products/edit.js
@@ -1,3 +1,17 @@
+const { readJSON } = require("../../data")
+
 module.exports = (req,res) => {
 
+    const categories = readJSON('categories.json');
+    const products = readJSON('products.json');
+    const sections = readJSON('sections.json');
+    const id = req.params.id;
+
+    const product = products.find(product => product.id === id)
+
+    return res.render('productEdit',{
+        categories,
+        sections,
+        ...product
+    })
 }
