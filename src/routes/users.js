afded413cb7f448e545a7012527ca9fe4ab5c52d
CamilaGuerrero00 <camilag.main@gmail.com>
2023-08-27 14:58:54 -0300

    listar productos

--- src/routes/users.js
+++ src/routes/users.js
@@ -8,6 +8,7 @@ router.get('/register', usersController.register);
 router.get('/login', usersController.login);
 router.get('/productCart', usersController.productCart);
 router.get('/productDetail/:id', usersController.productDetail);
+router.get('/productList', usersController.productList);
 router.get('/addProduct', usersController.addProduct);
 router.get('/editProduct', usersController.editProduct);
 
