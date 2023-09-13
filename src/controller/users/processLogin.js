075b2e4fad686166da2edfa1b48a7b64148daf32
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-09-13 16:58:28 -0300

    update processLogin - header

--- src/controller/users/processLogin.js
+++ src/controller/users/processLogin.js
@@ -5,7 +5,7 @@ module.exports = (req,res) => {
     const errors = validationResult(req)
     if (errors.isEmpty()) {
         const users = readJSON('users.json')
-        //const (id,name,role) = users.find(user => user.email === req.body.email)
+        const {id,name,role} = users.find(user => user.email === req.body.email)
         req.session.userLogin = {
             id,
             name,
