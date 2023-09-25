ee5387e79145b98b070dad82c5479b0154fdbefe
Roberto Zalazar <servicios773@gmail.com>
2023-09-25 15:40:30 -0300

    dv

--- src/controller/products/update.js
+++ src/controller/products/update.js
@@ -1,31 +1,72 @@
-const fs = require('fs');
+
+const { validationResult } = require("express-validator");
 const { readJSON, writeJSON } = require("../../data");
 
+module.exports = (req, res) => {
+  const errors = validationResult(req);
+  const products = readJSON("products.json");
+
+  if (errors.isEmpty()) {
+    const { name, price, category, description, section, addres, date, serviceCharge } = req.body;
+
+    const productsModify = products.map((product) => {
+      if(product.id === +req.params.id){
+        
+
+        product.name = name
+        product.description = description
+        product.price = +price
+        product.category = category
+        product.section = section
+        product.address = addres
+        product.date = date
+        product.image = product.image
+        product.serviceCharge = serviceCharge
+      }
+
+      return product;
+    });
+
+    writeJSON(productsModify, "products.json");
+
+    return res.redirect('/users/productList');
+  } else {
+    const categories = readJSON("categories.json");
+    const sections = readJSON("sections.json");
+
+    const product = products.find((product) => product.id === req.params.id);
+    return res.render("productEdit", {
+      errors: errors.mapped(),
+      id: req.params.id,
+      name: req.body.name,
+            price: +req.body.price,
+            category: req.body.category,
+            description: req.body.description,
+            section: req.body.section,
+            address: req.body.address,
+            date: req.body.date,
+            image: req.file ? req.file.filename : '',
+            serviceCharge: +req.body.serviceCharge,
+            location: req.body.location,
+      categories,
+      sections,
+      ...product,
+    });
+  }
+};
 
 
-module.exports = (req,res) => {
-    const {name, price, category, description, section, address, date, image, serviceCharge, location} = req.body;
-    const products = readJSON('products.json');
 
-    const productsModify = products.map(product => {
-        if(product.id === +req.params.id){
-            req.file &&(fs.existsSync(`./public/images/products${product.image}`) && fs.unlinkSync(`./public/images/products/${product.image}`))
-            product.name = name
-            product.description = description
-            product.price = +price
-            product.category = category
-            product.section = section
-            product.address = address
-            product.date = date
-            product.location = location;
-            product.image = req.file ? req.file.filename : product.image;
-            product.serviceCharge = serviceCharge
-        }
 
-        return product
-    })
 
-    writeJSON(productsModify, 'products.json')
 
-    return res.redirect('/products/productList')
-}
+
+
+
+
+
+
+
+
+
+
--- src/routes/products.js
+++ src/routes/products.js
@@ -3,6 +3,7 @@ const productsController = require('../controller/productsController');
 const upload = require('../middlewares/upload');
 const adminCheck = require('../middlewares/adminCheck');
 const addProductValidator = require('../validations/addProductValidator');
+const editProductvalidator = require('../validations/editProductvalidator');
 
 var router = express.Router();
 
@@ -13,7 +14,7 @@ router
     .get('/addProduct', adminCheck, productsController.addProduct)
     .post('/add',[adminCheck,upload.single('image'),addProductValidator], productsController.create)
     .get('/edit/:id', adminCheck, productsController.edit)
-    .put('/update/:id', adminCheck, upload.single('image'), productsController.update)
+    .put('/update/:id', [adminCheck, editProductvalidator,upload.single('image')], productsController.update)
     .delete('/remove/:id', adminCheck, productsController.remove)
 
 module.exports = router;
