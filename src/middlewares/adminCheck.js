dd5b4545e7cb2d1865b21eb191875a400d3d7fd9
brisaguimaraz <brisaguimaraz01@gmail.com>
2023-09-29 01:51:52 -0600

    modifico y agrego estilos en varias vistas

--- src/middlewares/adminCheck.js
+++ src/middlewares/adminCheck.js
@@ -1,7 +1,7 @@
 
 module.exports = (req,res,next) => {
     if (!(req.session.userLogin && req.session.userLogin.role == "admin")) {
-        return res.redirect('/');
+        return res.redirect('/productList');
     }
     return next()
 }
