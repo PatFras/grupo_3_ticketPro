0506db7d6ba21c1afb8b6e2f32c96acce2886925
Roberto Zalazar <servicios773@gmail.com>
2023-10-11 19:03:18 -0300

    elne

--- src/database/seeders/20231011211901-productos.js
+++ src/database/seeders/20231011211901-productos.js
@@ -2,13 +2,14 @@
 const productsJson = require('../../data/products.json')
 
 
-const productsDB = productsJson.map(({name, price,description,address,image,category,location,serviceCharge}) => {
+const productsDB = productsJson.map(({name, price,description,address,image,category,location,serviceCharge,section}) => {
   return {
     name,
     price,
     address,
     categoryId : category === "cine" ? 1 : 2,
     description,
+    sectionsId : section === "Nuevo" ? 1 : 2,
     location,
     serviceCharge,
     image,
