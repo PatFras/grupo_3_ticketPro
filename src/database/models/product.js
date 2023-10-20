17e195bb731d74d4572728ba250b7c757d4cbe6e
brisaguimaraz <brisaguimaraz01@gmail.com>
2023-10-20 11:27:34 -0600

    agrego asociacion

--- src/database/models/product.js
+++ src/database/models/product.js
@@ -10,7 +10,15 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Product.belongsTo(models.Section, {
+        as : 'section',
+        foreignKey : 'sectionId'
+      });
+
+      Product.belongsTo(models.Category,{
+        as : 'category',
+        foreignKey : 'categoryId'
+      });
     }
   }
   Products.init({
--- src/database/models/section.js
+++ src/database/models/section.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class sctions extends Model {
+  class Section extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,11 +13,11 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  sctions.init({
+  Section.init({
     name: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'sctions',
+    modelName: 'Section',
   });
-  return sctions;
+  return Section;
 };
