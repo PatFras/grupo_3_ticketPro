6124a30b3d9934963c176d0dc6f89a20db16adb6
Roberto Zalazar <servicios773@gmail.com>
2023-10-25 13:31:07 -0300

    editProduct

--- src/controller/products/update.js
+++ src/controller/products/update.js
@@ -1,58 +1,74 @@
 const { validationResult } = require("express-validator");
-const { readJSON, writeJSON } = require("../../data");
+const db = require("../../database/models");
 
-module.exports = (req, res) => {
-  const errors = validationResult(req);
-  const products = readJSON("products.json");
+module.exports = async (req, res) => {
+    const errors = validationResult(req);
 
-  if (errors.isEmpty()) {
-    const { name, price, category, description, section, address, date, serviceCharge } = req.body;
-    
+    if (errors.isEmpty()) {
+        const {
+            name,
+            price,
+            category,
+            description,
+            section,
+            address,
+            date,
+            serviceCharge,
+        } = req.body;
+        const productId = +req.params.id;
 
-    const productsModify = products.map((product) => {
-      if(product.id === +req.params.id){
-        
+        try {
+            await db.Product.update(
+                {
+                    name,
+                    description,
+                    price: +price,
+                    category,
+                    section,
+                    address,
+                    date,
+                    serviceCharge: +serviceCharge,
+                },
+                {
+                    where: {
+                        id: productId,
+                    },
+                }
+            );
 
-        product.name = name
-        product.description = description
-        product.price = +price
-        product.category = category
-        product.section = section
-        product.address = address
-        product.date = date
-        product.image = product.image
-        product.serviceCharge = serviceCharge
-      }
+            return res.redirect('/products/productList');
+        } catch (error) {
+            console.error(error);
+        }
+    } else {
+        try {
+            const categories = await db.Category.findAll();
+            const sections = await db.Section.findAll();
+            const productId = +req.params.id;
+            const product = await db.Product.findOne({
+                where: { id: productId },
+            });
 
-      return product;
-    });
-
-    writeJSON(productsModify, "products.json");
-
-    return res.redirect('/products/productList');
-  } else {
-    const categories = readJSON("categories.json");
-    const sections = readJSON("sections.json");
-
-    const product = products.find((product) => product.id === req.params.id);
-    return res.render("productEdit", {
-      errors: errors.mapped(),
-      id: req.params.id,
-      name: req.body.name,
-            price: +req.body.price,
-            category: req.body.category,
-            description: req.body.description,
-            section: req.body.section,
-            address: req.body.address,
-            date: req.body.date,
-            image: req.file ? req.file.filename : '',
-            serviceCharge: +req.body.serviceCharge,
-            location: req.body.location,
-      categories,
-      sections,
-      ...product,
-    });
-  }
+            return res.render("productEdit", {
+                errors: errors.mapped(),
+                id: req.params.id,
+                name: req.body.name,
+                price: +req.body.price,
+                category: req.body.category,
+                description: req.body.description,
+                section: req.body.section,
+                address: req.body.address,
+                date: req.body.date,
+                serviceCharge: +req.body.serviceCharge,
+                location: req.body.location,
+                categories,
+                sections,
+                ...product.dataValues,
+            });
+        } catch (error) {
+            console.error(error);
+        }
+    }
 };
 
 
