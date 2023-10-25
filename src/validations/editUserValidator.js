54a6728d8962333a1c2bf23d736ced5168613ac6
Roberto Zalazar <servicios773@gmail.com>
2023-10-25 11:36:27 -0300

    rbre

--- src/validations/editUserValidator.js
+++ src/validations/editUserValidator.js
@@ -1,42 +1,46 @@
 const { check, body } = require("express-validator");
 const db = require('../database/models');
-const { compareSync } = require('bcryptjs');
+
 module.exports = [
-  
   check("name")
-    .isLength({
-      min: 2,
-    })
-    .withMessage("El user name es obligatorio")
-    .trim()
+    .isLength({ min: 2 })
+    .withMessage("El nombre es obligatorio")
     .isAlpha("es-ES")
     .withMessage("Solo letras"),
+
   check("email")
-    .trim()
     .notEmpty()
     .withMessage("El email es obligatorio")
     .isEmail()
-    .withMessage("Formato inválido")
-   ,
-   body("password")
-  .custom((value, {req}) => {
-    const users = readJSON('users.json');
-    const user = users.find(user => user.id == req.session.userLogin.id);
-    if(!user || !compareSync(value,user.password)){
-      return false
-    }
-      return true
-  }).withMessage('Credenciales inválidas')
-  .isLength({
-    min: 6,
-    max: 12,
-  })
-  .withMessage('La contraseña debe contar con un minimo de 6 caracteres y maximo de 12'),
+    .withMessage("Formato inválido"),
+
+  body("password")
+    .custom(async (value, { req }) => {
+      const user = await db.User.findByPk(req.session.userLogin.id);
+
+      if (!user) {
+        throw new Error('Usuario no encontrado');
+      }
+
+      
+      if (user.password !== value) {
+        throw new Error('Credenciales inválidas');
+      }
+
+  
+      if (value.length < 6 || value.length > 12) {
+        throw new Error('La contraseña debe tener entre 6 y 12 caracteres');
+      }
+
+      return true; 
+    }),
+
   body('password2')
-    .custom((value,{req}) => {
-        if(value !== req.body.password){
-            return false
-        }
-        return true
-    }).withMessage('Las contraseñas no coinciden')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Las contraseñas no coinciden');
+      }
+
+      return true; 
+    })
 ];
