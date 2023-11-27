7baee82df25d4d8290dc4001d3dca90fb6dded1e
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-11-27 14:29:54 -0300

    update API users

--- src/controller/APIs/usersApiController.js
+++ src/controller/APIs/usersApiController.js
@@ -22,22 +22,19 @@ const checkEmail = async (req, res) => {
 
 const usersController = async (req, res) => {
   try {
-    // Obtener la lista completa de usuarios
     const users = await db.User.findAll({
       attributes: ["id", "name", "email"],
     });
 
-    // Obtener la cantidad total de usuarios
     const count = await db.User.count();
 
-    // Construir el objeto literal de respuesta para api/users/
     const usersData = {
       count: count,
       users: users.map((user) => ({
         id: user.id,
         name: user.name,
         email: user.email,
-        detail: `/api/users/${user.id}`, // Ajustar la ruta según tu implementación
+        detail: `/api/users/${user.id}`,
       })),
     };
 
@@ -58,9 +55,8 @@ const userController = async (req, res) => {
   const userId = req.params.id;
 
   try {
-    // Obtener los detalles de un usuario por ID
     const user = await db.User.findByPk(userId, {
-      attributes: { exclude: ["password", "category"] }, // Excluir información sensible
+      attributes: { exclude: ["password", "category"] },
     });
 
     if (!user) {
@@ -70,13 +66,11 @@ const userController = async (req, res) => {
       });
     }
 
-    // Construir el objeto literal de respuesta para api/users/:id
     const userDetails = {
       id: user.id,
       name: user.name,
       email: user.email,
-      // Agregar más propiedades según tu modelo de usuario
-      profileImage: `/api/users/${user.id}/image`, // Ajustar la ruta según tu implementación
+      profileImage: `/api/users/${user.id}/image`,
     };
 
     res.status(200).json({
