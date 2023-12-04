e0795ee276a5c8053a411817370a82e6819a8658
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-12-04 12:02:21 -0300

    redirección user "admin" a panel de administración

--- src/controller/users/processLogin.js
+++ src/controller/users/processLogin.js
@@ -6,26 +6,35 @@ module.exports = (req, res) => {
 
   if (errors.isEmpty()) {
     db.User.findOne({
-      where : {
-        email : req.body.email
-      }
+      where: {
+        email: req.body.email,
+      },
     })
-    .then(user => {
-      req.session.userLogin = {
-        id : user.id,
-        name : user.name,
-        role : user.roleId,
-      };
-      req.body.remember !== undefined &&
-      res.cookie("ticketProUser", req.session.userLogin, {
-        maxAge: 5000 * 60,
-      });
+      .then((user) => {
+        req.session.userLogin = {
+          id: user.id,
+          name: user.name,
+          role: user.roleId,
+        };
+        req.body.remember !== undefined &&
+          res.cookie("ticketProUser", req.session.userLogin, {
+            maxAge: 5000 * 60,
+          });
 
-      return res.redirect("/");
-    }).catch(error => console.log(error)) 
-  }else {
-    return res.render('users/login', {
-      errors : errors.mapped()
-    })
-  }  
+        if (user.roleId === 1) {
+          return res.redirect("/products/productList");
+        } else if (user.roleId === 2) {
+          req.body.remember !== undefined &&
+            res.cookie("ticketProUser", req.session.userLogin, {
+              maxAge: 5000 * 60,
+            });
+          return res.redirect("/");
+        }
+      })
+      .catch((error) => console.log(error));
+  } else {
+    return res.render("users/login", {
+      errors: errors.mapped(),
+    });
+  }
 };
