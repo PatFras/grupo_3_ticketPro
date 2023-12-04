da6e98e5e30f94223a9def6fa4bd09ba440e9e4e
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-12-04 14:28:54 -0300

    formateo de "date" con moment

--- src/controller/products/list.js
+++ src/controller/products/list.js
@@ -1,9 +1,20 @@
-const db = require('../../database/models');
+const db = require("../../database/models");
+const moment = require("moment");
 
-module.exports = async(req,res) => {
-    let products = await db.Product.findAll();   
-    return res.render('productList',{
-        products,
-        
-    })
-}
+module.exports = async (req, res) => {
+  try {
+    let products = await db.Product.findAll();
+
+    products = products.map((product) => ({
+      ...product.dataValues,
+      date: moment(product.date).format("DD-MM-YYYY"),
+    }));
+
+    return res.render("productList", {
+      products,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).send("Error al obtener los productos");
+  }
+};
