e78ef57bd86753157271d6a8fd94980423984f00
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-12-05 14:58:15 -0300

    update productDetail + función link a google map

--- src/controller/products/detail.js
+++ src/controller/products/detail.js
@@ -1,13 +1,15 @@
-const db = require('../../database/models');
-const moment = require('moment');
+const db = require("../../database/models");
+const moment = require("moment");
+const generateGoogleMapsLink = require("../../../public/js/googleMapsLink");
 
-module.exports = (req,res) => {
-    db.Product.findByPk(req.params.id)
-    .then(product => {
-        return res.render('productDetail',{
-			...product.dataValues,
-            moment
-		})
-    }).catch(error => console.log(error))
-    
-}
+module.exports = (req, res) => {
+  db.Product.findByPk(req.params.id)
+    .then((product) => {
+      return res.render("productDetail", {
+        ...product.dataValues,
+        moment,
+        generateGoogleMapsLink,
+      });
+    })
+    .catch((error) => console.log(error));
+};
