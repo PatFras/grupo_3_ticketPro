638a75884398b06e494c4fc7d7c96771d15d57bf
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-12-05 17:44:39 -0300

    update css

--- src/middlewares/upload.js
+++ src/middlewares/upload.js
@@ -1,17 +1,20 @@
-const multer = require('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination : (req,file,cb) => {
-        return cb(null, './public/images/products')
-    },
-    filename : (req,file,cb) => {
-        return cb(null, `${Date.now()}_products_${path.extname(file.originalname)}`)
-    }
-})
+  destination: (req, file, cb) => {
+    return cb(null, "./public/images/products");
+  },
+  filename: (req, file, cb) => {
+    return cb(
+      null,
+      `${Date.now()}_products_${path.extname(file.originalname)}`
+    );
+  },
+});
 
-const upload = multer({ 
-    storage 
-})
+const upload = multer({
+  storage,
+});
 
-module.exports = upload
+module.exports = upload;
