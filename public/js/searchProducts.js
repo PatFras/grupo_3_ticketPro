7fe023ac8013b07a9773e5f2d0f913dce0c6805e
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-12-06 13:52:11 -0300

    busqueda por provincia

--- public/js/searchProducts.js
+++ public/js/searchProducts.js
@@ -1,33 +1,29 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const form = document.getElementById('searchForm');
-    const keywordsInput = document.getElementById('busqueda');
-    const errorContainer = document.getElementById('searchError');
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("searchForm");
+  const keywordsInput = document.getElementById("busqueda");
+  const errorContainer = document.getElementById("searchError");
 
-    form.addEventListener('submit', function (event) {
-        const keywords = keywordsInput.value.trim();
+  form.addEventListener("submit", function (event) {
+    const keywords = keywordsInput.value.trim();
 
-        // Limpiar mensajes de error anteriores
-        errorContainer.textContent = ''; 
+    errorContainer.textContent = "";
 
-        if (keywords === '') {
-            event.preventDefault();
-            displayError('Búsqueda vacía');
-        }
-
-        if (keywords.length > 20) {
-            event.preventDefault();
-            displayError('El campo de búsqueda no puede tener más de 20 caracteres.');
-        }
-    });
-
-    function displayError(message) {
-        errorContainer.textContent = message;
+    if (keywords === "") {
+      event.preventDefault();
+      displayError("Ingresa una búsqueda");
     }
 
-    // Limpiar el mensaje de error cuando se cambia el contenido del campo de búsqueda
-    keywordsInput.addEventListener('input', function () {
-        errorContainer.textContent = '';
-    });
+    if (keywords.length > 20) {
+      event.preventDefault();
+      displayError("El campo de búsqueda no puede tener más de 20 caracteres.");
+    }
+  });
+
+  function displayError(message) {
+    errorContainer.textContent = message;
+  }
+
+  keywordsInput.addEventListener("input", function () {
+    errorContainer.textContent = "";
+  });
 });
-
-
--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -1,45 +1,55 @@
-const db = require('../database/models');
-const { Op } = require('sequelize');
-const moment = require('moment');
+const db = require("../database/models");
+const { Op } = require("sequelize");
+const moment = require("moment");
+const generateGoogleMapsLink = require("../../public/js/googleMapsLink");
 
 module.exports = {
-    index : (req,res) => {   
-        const products = db.Product.findAll()
-        const categories = db.Category.findAll()
+  index: (req, res) => {
+    const products = db.Product.findAll();
+    const categories = db.Category.findAll();
 
-        Promise.all([products,categories])
-        .then(([products,categories]) => {
-            return res.render('index', {
-                products,
-                categories,
-                moment
-            })
-        }).catch(error => console.log(error))
-    },
-    search: (req, res) => {
-        db.Product.findAll({
-            where : {
-                name : {
-                    [Op.substring] : req.query.keywords
-                }
-            }
-        })
-        .then(results => {
-            return res.render('searchResults', {
-                results,
-                keywords: req.query.keywords,
-                moment
-            });
-        })       
-    },
-    allEvents : (req,res) => {
-        const products = db.Product.findAll()
-        Promise.all([products])
-        .then(([products]) => {
-            return res.render('allEvents', {
-                products,
-                moment
-            })
-        })       
-    }
-}
+    Promise.all([products, categories])
+      .then(([products, categories]) => {
+        return res.render("index", {
+          products,
+          categories,
+          moment,
+        });
+      })
+      .catch((error) => console.log(error));
+  },
+  search: (req, res) => {
+    db.Product.findAll({
+      where: {
+        [Op.or]: [
+          {
+            name: {
+              [Op.substring]: req.query.keywords,
+            },
+          },
+          {
+            location: {
+              [Op.substring]: req.query.keywords,
+            },
+          },
+        ],
+      },
+    }).then((results) => {
+      return res.render("searchResults", {
+        results,
+        keywords: req.query.keywords,
+        moment,
+        generateGoogleMapsLink,
+      });
+    });
+  },
+  allEvents: (req, res) => {
+    const products = db.Product.findAll();
+    Promise.all([products]).then(([products]) => {
+      return res.render("allEvents", {
+        products,
+        moment,
+      });
+    });
+  },
+};
