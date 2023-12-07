44a90be85a8d8001b7dd9e32f3997c19c0ab7eb7
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-12-07 12:42:21 -0300

    creación modelos item-order-status + relaciones

--- src/database/models/status.js
+++ src/database/models/status.js
@@ -1,35 +1,24 @@
 "use strict";
 const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Status extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      User.belongsTo(models.Roles, {
-        as: "roles",
-        foreignKey: "roleId",
-      });
-
-      User.hasMany(models.Order, {
-        as: "orders",
-        foreignKey: "userId",
-      });
+      // define association here
     }
   }
-  User.init(
+  Status.init(
     {
       name: DataTypes.STRING,
-      email: DataTypes.STRING,
-      password: DataTypes.STRING,
-      roleId: DataTypes.INTEGER,
     },
     {
       sequelize,
-      modelName: "User",
+      modelName: "Status",
     }
   );
-  return User;
+  return Status;
 };
