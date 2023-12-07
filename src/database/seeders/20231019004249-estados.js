4f3c9ab9c973b6798efa3bd62ebb03a96c72b6a0
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-12-07 14:24:30 -0300

    seed estados + order por cada user

---
+++ src/database/seeders/20231019004249-estados.js
@@ -0,0 +1,27 @@
+"use strict";
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "Statusses",
+      [
+        {
+          name: "open",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          name: "close",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Statusses", null, {});
+  },
+};
