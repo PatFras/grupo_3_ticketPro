0553c57bd41cfd3c1759440deed42749f34bd768
Patricio Frascaroli <pmfrascaroli@gmail.com>
2023-12-08 20:53:19 -0300

    update carrito

--- src/controller/productsController.js
+++ src/controller/productsController.js
@@ -1,14 +1,9 @@
-
-
 module.exports = {
-    productCart : (req,res) => {
-        return res.render('productCart')
-    },
-    productDetail : require('./products/detail'),
-    productList : require('./products/list'),
-    addProduct :  require('./products/add'),
-    create: require('./products/create'),
-    edit : require('./products/edit'),
-    update: require('./products/update'),
-    remove: require('./products/remove'),
-}
+  productDetail: require("./products/detail"),
+  productList: require("./products/list"),
+  addProduct: require("./products/add"),
+  create: require("./products/create"),
+  edit: require("./products/edit"),
+  update: require("./products/update"),
+  remove: require("./products/remove"),
+};
